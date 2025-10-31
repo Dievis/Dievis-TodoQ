a843119e9d3bffdf94f4d32fc908a010535f8c4c
Dievis <dattp3010@gmail.com>
2025-10-31 12:10:32 +0700

    update server backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,26 +1,37 @@
-import express from 'express';
-import tasksRouter from './routes/tasksRouters.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
+import express from "express";
+import taskRoute from "./routes/tasksRouters.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
 
+// middlewares
 app.use(express.json());
-app.use(cors());
 
-// app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
-app.use("/api/tasks",tasksRouter);
+app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-    console.log('Server chạy trên cổng', PORT);
+  app.listen(PORT, () => {
+    console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
 
